ded696324a7f8cb82a7bca587550e268bfa9299d
cbohren7 <cbohren@tamu.edu>
2017-11-30 14:25:46 -0600

    saved the customer mtnce as student so we can rename and reuuse

---
+++ StudentMaintenance/StudentMaintenance/MMABooksDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CustomerMaintenance
+{
+    public static class MMABooksDB
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\MMABooks.mdf;" +
+                "Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
+//they are using microsoft SQL NOT MySQL
+// should use MySql.Data.MySqlClient
+// change every connection string to MySQL
+// sometimes User id = UID and password = pwrd
